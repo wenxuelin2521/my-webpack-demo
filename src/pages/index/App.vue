3321dc8a11809f9d3ec33374c48ca39cf40be67b
温学林 <wenxl@minstone.com.cn>
2019-05-16 19:27:54 +0800

    提交修改:修改目录

---
+++ src/pages/index/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root">
+    <h2 class="abc">
+      app
+    </h2>
+    <img src="../../images/test.png" alt=""/>
+    
+    
+    <div class="box">
+        123
+    </div>
+    
+    <h3>test</h3>
+    
+    
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+h2{
+    background: red;
+}
+
+h3{
+    background-color: hotpink;
+}
+.box{
+    margin: 20px;
+    width: 200px;
+    height: 200px;
+    background-image: url('~@/images/test.png')
+}
+</style>
---
+++ src/pages/index/main.js
@@ -0,0 +1,10 @@
+import Vue from 'Vue'
+import App from './App.vue'
+
+import '@/styles/normalize.scss'
+import '@/styles/base.scss'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
